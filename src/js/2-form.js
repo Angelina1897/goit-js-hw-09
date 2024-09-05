f2aa79fd7cb3fc47fa3d0d9e231b516402911317
G-Angelina <168190069+G-Angelina@users.noreply.github.com>
2024-09-05 21:49:42 +0200

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,53 @@
-.console.log(Form);
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: ''
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+function onFormInput(event) {
+
+  formData[event.target.name] = event.target.value.trim();
+
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormFromStorage() {
+ 
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    if (formData.email) {
+      emailInput.value = formData.email;
+    }
+    if (formData.message) {
+      messageInput.value = formData.message;
+    }
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+
+  console.log('Form data:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = { email: '', message: '' };
+}
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+populateFormFromStorage();
